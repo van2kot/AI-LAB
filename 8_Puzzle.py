f664ca2076a4a7c695021780e8a41c22e050473e
van2kot <117808297+van2kot@users.noreply.github.com>
2023-12-06 15:16:01 +0530

    Update 8_Puzzle.py

--- 8_Puzzle.py
+++ 8_Puzzle.py
@@ -1,79 +1,73 @@
-def bfs(src,target):
-    queue = []
-    queue.append(src)
-    
-    exp = []
-    
-    while len(queue) > 0:
-        source = queue.pop(0)
-        exp.append(source)
-        
-        print(source)
-        
-        if source==target:
-            print("success")
-            return
-        
-        poss_moves_to_do = []
-        poss_moves_to_do = possible_moves(source,exp)
-        
-        for move in poss_moves_to_do:
-            
-            if move not in exp and move not in queue:
-                queue.append(move)
-def possible_moves(state,visited_states): 
-    #index of empty spot
-    b = state.index(0)
-    
-    #directions array
-    d = []
-    #Add all the possible directions
-    
-    if b not in [0,1,2]: 
-        d.append('u')
-    if b not in [6,7,8]: 
-        d.append('d')
-    if b not in [0,3,6]: 
-        d.append('l')
-    if b not in [2,5,8]: 
-        d.append('r')
-        
 
-    # If direction is possible then add state to move
-    pos_moves_it_can = []
-    
-    # for all possible directions find the state if that move is played
-    ### Jump to gen function to generate all possible moves in the given directions
-    
-    for i in d:
-        pos_moves_it_can.append(gen(state,i,b))
-        
-    return [move_it_can for move_it_can in pos_moves_it_can if move_it_can not in visited_states]
-def gen(state, m, b):
-    temp = state.copy()                              
-    
-    if m=='d':
-        temp[b+3],temp[b] = temp[b],temp[b+3]
-    
-    if m=='u':
-        temp[b-3],temp[b] = temp[b],temp[b-3]
-    
-    if m=='l':
-        temp[b-1],temp[b] = temp[b],temp[b-1]
-    
-    if m=='r':
-        temp[b+1],temp[b] = temp[b],temp[b+1]
-        
-    
-    # return new state with tested move to later check if "src == target"
-    return temp
-# src = [1,2,3,4,5,6,0,7,8]
-# target = [1,2,3,4,5,6,7,8,0]    
+# Class representing the 8 Puzzle problem
+class Puzzle8:
+    def __init__(self, size=3):
+        self.size = size
 
-src = [1,2,3,0,4,5,6,7,8] 
-target = [1,2,3,4,5,0,6,7,8]  
+    def display_state(self, state):
+        for i in range(self.size):
+            for j in range(self.size):
+                print(state[i * self.size + j], end=" ")
+            print()
 
-src= [2,0,3,1,8,4,7,6,5] 
-target=[1,2,3,8,0,4,7,6,5]
-   
-bfs(src, target)
+    def get_blank_index(self, state):
+        return state.index(-1)
+
+    def get_neighbors(self, state):
+        neighbors = []
+        blank_index = self.get_blank_index(state)
+        row, col = divmod(blank_index, self.size)
+
+        moves = [(0, 1), (1, 0), (0, -1), (-1, 0)]  # right, down, left, up
+
+        for move in moves:
+            new_row, new_col = row + move[0], col + move[1]
+
+            if 0 <= new_row < self.size and 0 <= new_col < self.size:
+                new_state = state[:]
+                new_blank_index = new_row * self.size + new_col
+
+                # Swap the blank tile with the neighbor
+                new_state[blank_index], new_state[new_blank_index] = new_state[new_blank_index], new_state[blank_index]
+
+                neighbors.append(new_state)
+
+        return neighbors
+
+    def is_goal_state(self, state, target_state):
+        return state == target_state
+
+    def bfs(self, initial_state, target_state):
+        queue = deque([(initial_state, [])])
+        visited = set()
+
+        while queue:
+            current_state, path = queue.popleft()
+
+            if self.is_goal_state(current_state, target_state):
+                return path
+
+            if tuple(current_state) not in visited:
+                visited.add(tuple(current_state))
+                neighbors = self.get_neighbors(current_state)
+
+                for neighbor in neighbors:
+                    queue.append((neighbor, path + [neighbor]))
+
+        return None
+
+# Example usage:
+initial_state = [1, 2, 3, 4, -1, 5, 6, 7, 8]
+goal_state = [1, 2, 3, 4, 5, 6, 7, 8, -1]
+
+puzzle = Puzzle8()
+solution = puzzle.bfs(initial_state, goal_state)
+
+if solution:
+    print("Solution found:")
+    for step, state in enumerate(solution):
+        print(f"Step {step + 1}:")
+        puzzle.display_state(state)
+        print()
+else:
+    print("No solution found.")
